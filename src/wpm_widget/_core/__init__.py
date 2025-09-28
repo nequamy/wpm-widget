dd31b78f063cc1e134392ffe717e944abd1bef1d
nequamy <nequamy@gmail.com>
2025-09-28 21:36:13 +0300

    [~] create base classes inside all paths and put it into __init__

---
+++ src/wpm_widget/_core/__init__.py
@@ -0,0 +1,5 @@
+from wpm_widget._core.data_storage import DataStorage
+from wpm_widget._core.keyboard_monitor import KeyboardMonitor
+from wpm_widget._core.wpm_calculator import WPMCalculator
+
+__all__: list[str] = ["DataStorage", "KeyboardMonitor", "WPMCalculator"]
---
+++ src/wpm_widget/gui/__init__.py
@@ -0,0 +1,4 @@
+from wpm_widget.gui.menubar import MenuBAR
+from wpm_widget.gui.widget import Widget
+
+__all__: list[str] = ["MenuBAR", "Widget"]
